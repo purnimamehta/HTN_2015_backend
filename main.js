e3009ddf7626d8d86cb075d874d101a0f2313802
Snaheth <thumathysnaheth@gmail.com>
2015-09-19 04:02:24 -0400

    firebase setup

---
+++ main.js
@@ -0,0 +1,24 @@
+// <script>
+//   window.fbAsyncInit = function() {
+//     FB.init({
+//       appId      : '1634407773470693',
+//       xfbml      : true,
+//       version    : 'v2.4'
+//     });
+//   };
+
+//   (function(d, s, id){
+//      var js, fjs = d.getElementsByTagName(s)[0];
+//      if (d.getElementById(id)) {return;}
+//      js = d.createElement(s); js.id = id;
+//      js.src = "//connect.facebook.net/en_US/sdk.js";
+//      fjs.parentNode.insertBefore(js, fjs);
+//    }(document, 'script', 'facebook-jssdk'));
+// </script>
+
+
+var Firebase = require('firebase');
+var firebaseURL = new Firebase("htnb.firebaseIO.com");
+
+var fbgraph = require("fbgraph");
+fbgraph.setAccessToken(1634407773470693);
